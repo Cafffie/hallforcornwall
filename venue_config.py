bda93fa643a1bd10c2093bc0204d3802fbfd456b
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-07-24 16:37:43 +0100

    Update venue_config.py

--- venue_config.py
+++ venue_config.py
@@ -9,7 +9,7 @@ PAGES = [
     (f"{BASE_URL}whats-on/?category=plays-drama", "Play"),
     (f"{BASE_URL}whats-on/?category=musical-theatre", "Musical"),
     (f"{BASE_URL}whats-on/?category=christmas-show", "Musical"),
-    (f"{BASE_URL}whats-on/?category=christmas-show", "Musical"),
+    (f"{BASE_URL}whats-on/?category=children-family", "Musical"),
 ]
 
 COOKIE_BTN_XPATH = (
@@ -19,31 +19,35 @@ COOKIE_BTN_XPATH = (
 ADDRESS_URL = "https://www.hallforcornwall.co.uk/visiting-us/"
 
 DEFAULT_THEATRE_DETAILS = {
-    "venue": "Cornwall Play House",
+    "venue": "Cornwall Playhouse",
     "address": "Hall for Cornwall, Back Quay, Truro TR1 2LL",
-    "city": "Truro",
+    "city": "Back Quay",
     "country": "UK",
 }
 
 VENUE_MAP = {
     "princess pavilion": {
         "venue": "Princess Pavilion",
-        "address": None,
+        "address": "41 Melvill Road, Falmouth, Cornwall TR11 4AR, United Kingdom",
         "city": "Falmouth",
         "country": "United Kingdom",
     },
-
     "truro school concert hall": {
         "venue": "Truro School Concert Hall",
-        "address": None,
+        "address": "Trennick Lane, Truro, Cornwall TR1 1TH",
         "city": "Truro",
         "country": "United Kingdom",
     },
-
     "truro school": {
         "venue": "Truro School",
-        "address": None,
+        "address": "Trennick Lane, Truro, Cornwall TR1 1TH",
         "city": "Truro",
+        "country": "United Kingdom",
+    },
+    "creation works": {
+        "venue": "Creation Works",
+        "address": "13a Moorland Road, Par, Cornwall PL24 2PA",
+        "city": "Par",
         "country": "United Kingdom",
     },
 }
@@ -51,10 +55,10 @@ VENUE_MAP = {
 SELECTORS = {
     "cookie_button": "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']",
     "theatre_address_xpath": "//h2[normalize-space()='Address']/following-sibling::p[1]",
+    "event_description": "div.whatson-event-details-content",
     "shows_link": ".WhatsonItem .WhatsOnDateBnt a:first-of-type",
     "title": ".whatson-event-detailsHeading-title h1",
     "terminal_date": ".EventDetailHeading_row span.EventpostDate",
-    "event_description": "div.whatson-event-details-content",
     "first_book_btn": ".BannerBookBtn",
     "date_blocks": "tbody tr",
     "booking_url": ".BookingList_btn a",
